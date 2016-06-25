b01710f780356cc9afe9d34e59a03ed995adb71c
alexr1337 <alex-reichenbach@gmx.de>
2016-06-25 03:09:28 +0200

    Add files via upload

---
+++ Dozent.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Alexander on 25.06.2016.
+ */
+public class Dozent implements Reservierer {
+
+    String name;
+
+    public Dozent (String name) {
+        this.name = name;
+    }
+}
