f1ab8ee88a7f91b5800687e8a40060af8a713dfa
alexr1337 <alex-reichenbach@gmx.de>
2016-07-13 02:39:34 +0200

    Update StandardNutzer.java

--- StandardNutzer.java
+++ StandardNutzer.java
@@ -2,22 +2,30 @@ import java.util.ArrayList;
 
 public class StandardNutzer extends Nutzer implements Reservierer {
 
-	private ArrayList<Reservierung> reservierungen = new ArrayList<Reservierung>();
-	
-	public StandardNutzer (String name, String password, String sicherheitsFrage, String sicherheitsAntwort) {
-		super (name, password, sicherheitsFrage, sicherheitsAntwort);
-	}
+    private ArrayList<Reservierung> reservierungen = new ArrayList<Reservierung>();
 
-	public ArrayList<Reservierung> getReservierungen() {
-		return reservierungen;
-	}
+    public StandardNutzer (String name, String password, String sicherheitsFrage, String sicherheitsAntwort) {
+        super (name, password, sicherheitsFrage, sicherheitsAntwort);
+    }
 
-	public void addReservierung (Reservierung r) {
-		reservierungen.add(r);		// NICHT FERTIG
-									// Sortiertes Einfuegen fehlt
-	}
-	
-	public void setReservierungen(ArrayList<Reservierung> reservierungen) {
-		this.reservierungen = reservierungen;
-	}
+    public ArrayList<Reservierung> getReservierungen() {
+        return reservierungen;
+    }
+
+    public void addReservierung (Reservierung r) {
+        GlobaleMethoden.addReservierungtoArrayList(reservierungen, r);
+    }
+
+    public void setReservierungen(ArrayList<Reservierung> reservierungen) {
+        this.reservierungen = reservierungen;
+    }
+
+    public boolean istFrei(Zeitraum zr) {
+        if (findeKollision(zr)==null) return true;
+        else return false;
+    }
+
+    public Reservierung findeKollision (Zeitraum zr) {
+        return GlobaleMethoden.findeKollisioninArrayList(reservierungen, zr);
+    }
 }
