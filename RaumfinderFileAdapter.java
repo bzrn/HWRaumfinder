ae2989e7e0c1005a4bece46c3821ccea2333516d
alexr1337 <alex-reichenbach@gmx.de>
2016-08-11 23:27:07 +0200

    Vervollständigt mit resCounter

--- RaumfinderFileAdapter.java
+++ RaumfinderFileAdapter.java
@@ -4,18 +4,20 @@ import Verarbeitung.Raumfinder;
 import VerarbeitungInterfaces.RaumfinderIF;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class RaumfinderFileAdapter implements RaumfinderFileAdapterIF {
 
 	// Singleton-Implementierung
 	private static RaumfinderFileAdapter ourInstance = new RaumfinderFileAdapter();
 
-	private File datei;
+	private File datei, rescounterdatei;
 
 	// Singleton-Konstruktor
 	private  RaumfinderFileAdapter () {
 		new File ("FileSaver/").mkdir();
 		datei = new File ("FileSaver/persistenz.raumfind");
+		rescounterdatei = new File ("FileSaver/rescounter.raumfind");
 	}
 	
 	// Singleton-getInstance
@@ -33,14 +35,28 @@ public class RaumfinderFileAdapter implements RaumfinderFileAdapterIF {
 			System.err.println("Es gab ein Problem beim Speichern: IOException");
 			return;
 		}
+
+		long resCounter=Raumfinder.getInstance().getResCounter();
+		try {
+			FileWriter fileout = new FileWriter(rescounterdatei);
+			fileout.write(String.valueOf(resCounter));
+			fileout.close();
+		} catch(IOException e){
+			System.err.println("Es gab ein Problem beim Speichern des ResCounters: IOException");
+		}
 	}
 
 	@SuppressWarnings({ "resource", "static-access" })
 	public RaumfinderIF load() {
 
 		try {
+			Scanner sc = new Scanner(rescounterdatei);
+			long resCounter = sc.nextLong();
+			sc.close();
 			ObjectInputStream in = new ObjectInputStream (new FileInputStream(datei));
-			return (RaumfinderIF) in.readObject();
+			RaumfinderIF rfif = (RaumfinderIF) in.readObject();
+			rfif.setResCounter(resCounter);
+			return rfif;
 		} catch (IOException e) {
 			System.err.println("Es gab ein Problem beim Einlesen des ResCounters: IOException");
 		} catch (ClassNotFoundException t){
