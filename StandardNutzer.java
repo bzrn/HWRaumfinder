ef9584f4acfb5a6bbf073703962c753d9277ed7d
agesb <hanna.behnke@gmail.com>
2016-08-12 21:56:46 +0200

    Update Unit-Spezifikationen

--- StandardNutzer.java
+++ StandardNutzer.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
- * <strong/>Zweck:</strong> StandardNutzer-Objekten können im Gegensatz zu Objekten der Mutterklasse "Nutzer" Reservierungen zugeorndet werden. Diese werden als Liste gespeichert.
+ * <strong/>Zweck:</strong> Ermöglicht das Erstellen von StandardNutzer-Objekten, denen, im Gegensatz zu Objekten der Mutterklasse "Nutzer", Reservierungen zugeorndet werden können. Diese werden als Liste gespeichert.
  * <p><strong>Änderungshistorie:</strong> </p>
  * @version 2.3
  * @author Alexander Reichenbach
@@ -15,8 +15,12 @@ import java.util.ArrayList;
 
 public class StandardNutzer extends Nutzer implements VerarbeitungInterfaces.StandardNutzerIF, Reservierer, Serializable {
 
-    private ArrayList<Reservierung> reservierungen = new ArrayList<Reservierung>();
+   // Attribute
+	private static final long serialVersionUID = -6698216101275037529L;
+	
+	private ArrayList<Reservierung> reservierungen = new ArrayList<Reservierung>();
 
+	// Konstruktor
     public StandardNutzer (String name, String password, String sicherheitsFrage, String sicherheitsAntwort) {
         super (name, password, sicherheitsFrage, sicherheitsAntwort);
     }
@@ -25,15 +29,23 @@ public class StandardNutzer extends Nutzer implements VerarbeitungInterfaces.Sta
         return reservierungen;
     }
 
+    
+    /**
+     * <p><strong>Vorbedingungen:</strong> Es muss eine Reservierung übergeben werden und es muss einen Standardnutzer geben, auf den die Methode angewendet werden kann.</p>
+	 * <p><strong>Effekt:</strong> Fügt das Reservierung sortiert in die Reservierungsliste ein, falls es nicht ohnehin schon in der Liste vorhanden ist.</p>
+     * @param r Reservierung, die der Reservierungsliste des Nutzers hinzugefügt werden soll
+     * @return <strong>true</strong> wenn die Reservierung der Liste hinzugefügt wurde, <strong>false</strong> wenn die Reservierung nicht hinzugefügt wurde, da sie bereits in der Liste vorhanden war
+     */
     public boolean addReservierung (Reservierung r) {
         if (!reservierungen.contains(r)) {
-            GlobaleMethoden.addReservierungtoArrayList(reservierungen, r);
+            GlobaleMethoden.addReservierungtoArrayList(reservierungen, r); 
             return true;
         } else {
             return false;
         }
     }
 
+    //entfernt eine Reservierung aus der Reservierungsliste
     public void removeReservierung (Reservierung weg) {
     	reservierungen.remove(weg);
     }
@@ -42,33 +54,25 @@ public class StandardNutzer extends Nutzer implements VerarbeitungInterfaces.Sta
         this.reservierungen = reservierungen;
     }
 
+   
+    /**
+     * <p><strong>Vorbedingungen:</strong> Es muss ein Zeitraum übergeben werden und es muss einen Standardnutzer geben, auf den die Methode angewendet wird.</p>
+	 * <p><strong>Effekt:</strong> Prüft, ob der Zeitraum mit einer der Reservierungen des Nutzers kollidiert.</p>
+     * @param zr Zeitraum, der auf Kollision mit den Reservierungen des Nutzers geprüft wird
+     * @return <strong>true</strong> wenn Zeitraum und Reservierungen nicht kollidieren, <strong>false</strong> wenn sie kollidieren
+     */
     public boolean istFrei(Zeitraum zr) {
         if (findeKollision(zr)==null) return true;
         else return false;
     }
 
+    /**
+     * <p><strong>Vorbedingungen:</strong> Es muss ein Zeitraum übergeben werden und es muss einen Standardnutzer geben, auf den die Methode angewendet wird.</p>
+	 * <p><strong>Effekt:</strong> Prüft, ob der Zeitraum mit einer der Reservierungen des Nutzers kollidiert.</p>
+     * @param zr Zeitraum, der auf Kollision mit den Reservierungen des Nutzers geprüft wird
+     * @return das Reservierungsobjekt, mit dem der Zeitraum kollidiert, wenn eine Kollision vorliegt und "null", wenn keine Kollision vorliegt
+     */
     public Reservierung findeKollision (Zeitraum zr) {
         return GlobaleMethoden.findeKollisioninArrayList(reservierungen, zr);
     }
-
-    /*
-    // Serializable-Implementierung
-
-    private void writeObject(ObjectOutputStream oos) throws IOException {
-        // default serialization
-        oos.defaultWriteObject();
-        // write the object
-        //Reservierung[] resSave = new Reservierung [reservierungen.size()];
-        //reservierungen.toArray(resSave);
-        //oos.writeObject(resSave);
-    }
-
-    private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
-        // default deserialization
-        ois.defaultReadObject();
-        // read the objects
-        //Reservierung[] resLoad = (Reservierung[])ois.readObject();
-        //reservierungen = new ArrayList<Reservierung> (Arrays.asList(resLoad));
-    }
-    */
 }
