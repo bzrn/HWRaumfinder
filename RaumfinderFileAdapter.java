50a742821150733d9abbf40c9805865defc866f0
agesb <hanna.behnke@gmail.com>
2016-08-12 23:28:09 +0200

    Unit-Spezifikationen

--- RaumfinderFileAdapter.java
+++ RaumfinderFileAdapter.java
@@ -6,6 +6,13 @@ import VerarbeitungInterfaces.RaumfinderIF;
 import java.io.*;
 import java.util.Scanner;
 
+/**
+ * <strong>Zweck:</strong> Realisiert das Speichern und Laden des Programmes mit allen angelegten Räumen, Nutzern und Reservierungen
+ * <p><strong>Änderungshistorie:</strong></p>
+ * @version 3.2
+ * @author Hanna Behnke, Alexander Reichenbach
+ *
+ */
 public class RaumfinderFileAdapter implements RaumfinderFileAdapterIF {
 
 	// Singleton-Implementierung
@@ -25,9 +32,16 @@ public class RaumfinderFileAdapter implements RaumfinderFileAdapterIF {
     	return ourInstance;
     }
 
+    /**
+	 * <p><strong>Vorbedingungen:</strong> Es muss einen Raumfinder geben, auf den die Methode angewendet wird.</p>
+	 * <p><strong>Effekt:</strong> Der Raumfinder wird serialisiert und somit im Ganzen als ein Objekt in einer Datei gespeichert. 
+	 * Der statische "resCounter" wird in einer gesonderten Datei gespeichert, da er nicht serialisierbar ist.</p>
+	 *
+	 */
 	@SuppressWarnings({ "resource", "static-access" })
 	public void save(){
-
+		
+		//Speichern des Raumfinders
 		try {
 			ObjectOutputStream out = new ObjectOutputStream (new FileOutputStream(datei));
 			out.writeObject((RaumfinderIF)Raumfinder.getInstance());
@@ -36,6 +50,7 @@ public class RaumfinderFileAdapter implements RaumfinderFileAdapterIF {
 			return;
 		}
 
+		// Speichern des statischen resCounters in einer gesonderten Datei 
 		long resCounter=Raumfinder.getInstance().getResCounter();
 		try {
 			FileWriter fileout = new FileWriter(rescounterdatei);
@@ -46,13 +61,21 @@ public class RaumfinderFileAdapter implements RaumfinderFileAdapterIF {
 		}
 	}
 
+	/**
+	 * <p><strong>Vorbedingungen:</strong> Um Fehlermeldungen zu vermeiden, sollte eine einlesbare Datei mit dem serialisierten Raumfinder existieren.</p>
+	 * <p><strong>Effekt:</strong> Liest das Raumfinder-Interface und den resCounter ein.</p>
+	 * @return gibt <strong>null</strong> zurück, wenn ein Fehler beim Einlesen auftritt, sonst wird das RaumfinderIF zurückgegeben
+	 */
 	@SuppressWarnings({ "resource", "static-access" })
 	public RaumfinderIF load() {
-
+		
+		// Einlesen des Raumfinder-Interfaces
 		try {
 			Scanner sc = new Scanner(rescounterdatei);
 			long resCounter = sc.nextLong();
 			sc.close();
+			
+			// Einlesen des resCounters
 			ObjectInputStream in = new ObjectInputStream (new FileInputStream(datei));
 			RaumfinderIF rfif = (RaumfinderIF) in.readObject();
 			rfif.setResCounter(resCounter);
