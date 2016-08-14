c52f254838e095a463d2dec3f76404dc05954abc
bzrn <s_zerrahn@stud.hwr-berlin.de>
2016-08-14 12:39:18 +0200

    Änderungshistorie

--- Ausstattung.java
+++ Ausstattung.java
@@ -4,8 +4,76 @@ import java.io.Serializable;
 
 
 /**
- * <strong/>Zweck:</strong> Ermöglicht es, Ausstattungsobjekte zu erstellen, die Ausstattung und Kapazität (eines Raumes) beschreiben
- * <p><strong>Änderungshistorie:</strong> ...</p>
+ * <strong>Zweck:</strong> Ermoeglicht es, Ausstattungsobjekte zu erstellen, die Ausstattung und Kapazitaet eines Raumes beschreiben
+ * <h2>Aenderungshistorie:</h2>
+ * 
+ * <ol>
+ * 		<li>
+ * 			<ul>
+ * 				<li> <strong> Version: </strong> 0.1 </li>
+ *				<li> <strong> Datum: </strong> 25.06.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Initiale Befuellung </li>
+ *			 </ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 0.2 </li>
+ *				<li> <strong> Datum: </strong> 12.07.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Update: Neues Scoring </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 0.3 </li>
+ *				<li> <strong> Datum: </strong> 28.07.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Hinzufuegen von toStringArray() fuer Verarbeitung von GUI </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 1.0 </li>
+ *				<li> <strong> Datum: </strong> 02.08.16 </li>
+ *				<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 				<li> <strong> Beschreibung: </strong> Update: Implements Serializable </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 1.1 </li>
+ *				<li> <strong> Datum: </strong> 11.08.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Interfaces implementiert </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 1.2 </li>
+ *				<li> <strong> Datum: </strong> 11.08.16 </li>
+ *				<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 				<li> <strong> Beschreibung: </strong> Kommentare/Unit-Spezifikationen </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 2.0 </li>
+ *				<li> <strong> Datum: </strong> 12.08.16 </li>
+ *				<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 				<li> <strong> Beschreibung: </strong> Finales Update </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 2.1 </li>
+ *				<li> <strong> Datum: </strong> 13.08.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Bug Fixes </li>
+ *			</ul>
+ *		</li>
+ *
+ * </ol>
  * @version 2.1
  * @author Hanna Behnke, Alexander Reichenbach
  *
