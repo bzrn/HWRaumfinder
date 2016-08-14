3bb3624ffd073518eea69ed1e549b0a3e752ac61
bzrn <s_zerrahn@stud.hwr-berlin.de>
2016-08-14 12:27:08 +0200

    Änderungshistorie

--- Admin.java
+++ Admin.java
@@ -1,9 +1,55 @@
 package Verarbeitung;
 
 /**
- * <strong>Zweck:</strong> Gibt Administratoren zusätzlich zu den regulären Nutzer-Attributen die Eigenschaft, löschbar oder nicht löschbar zu sein
- * <p><strong>Änderungshistorie:</strong>...</p>
+ * <strong> Zweck:</strong> Gibt Administratoren zusaetzlich zu den regulaeren Nutzer-Attributen die Eigenschaft, loeschbar oder nicht loeschbar zu sein 
+ * <h2>Aenderungshistorie:</h2>
+ * 
+ * <ol>
+ * 		<li>
+ * 			<ul>
+ * 				<li> <strong> Version: </strong> 0.1 </li>
+ *				<li> <strong> Datum: </strong> 11.07.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Initiale Befuellung </li>
+ *			 </ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 0.2 </li>
+ *				<li> <strong> Datum: </strong> 28.07.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Update Package Verarbeitung </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 1.0 </li>
+ *				<li> <strong> Datum: </strong> 07.08.16 </li>
+ *				<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 				<li> <strong> Beschreibung: </strong> Finales Update </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 1.1 </li>
+ *				<li> <strong> Datum: </strong> 11.08.16 </li>
+ *				<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 				<li> <strong> Beschreibung: </strong> Kommentare/Unit-Spezifikationen </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 2.0 </li>
+ *				<li> <strong> Datum: </strong> 12.08.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Bug Fixes </li>
+ *			</ul>
+ *		</li>
+ *
+ * </ol>
+ * 
  * @version 2.0
+ * 
  * @author Alexander Reichenbach
  *
  */
